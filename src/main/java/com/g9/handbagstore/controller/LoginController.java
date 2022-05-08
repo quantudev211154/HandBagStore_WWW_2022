c4ac0bbc12381aed262bd08973297da03ad235af
quantudev211154 <quantu211154@gmail.com>
2022-05-08 22:17:20 +0700

    Update tum lum

--- src/main/java/com/g9/handbagstore/controller/LoginController.java
+++ src/main/java/com/g9/handbagstore/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.g9.handbagstore.controller;
 
 import com.g9.handbagstore.config.MyUserDetail;
+import com.g9.handbagstore.entity.User;
+import com.g9.handbagstore.service.UserService;
 import com.g9.handbagstore.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -11,22 +13,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 	@Autowired
-	private UserServiceImpl userService;
+	private UserService userService;
 	
 	@GetMapping("/welcome")
 	public String welcomePage(Model model) {
 		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		User user = null;
 
 		if (principal instanceof MyUserDetail){
 			String username = ((MyUserDetail) principal).getUsername();
+			user = userService.getUserByUserName(username);
 
-			String avatar = userService.getUserByUserName(username).getAvatar();
+			String tmpAvatar = userService.getUserByUserName(username).getAvatar();
+
+			String avatar = (tmpAvatar != null)
+					? tmpAvatar
+					: "";
 
 			if (!avatar.isEmpty()){
 				model.addAttribute("userAvatar", "data:image/png;base64," + avatar);
 			}
 		}
 
+		model.addAttribute("userLastName", user.getLastName());
 		model.addAttribute("pageTitle", "G9 Bag Store");
 
 		return "view_customer/index";
