a67a763d095fa4d790f646b842c96013e9028743
Vuong Lam <lamdavid821@gmail.com>
2022-05-09 21:02:55 +0700

    update impl

--- src/main/java/com/g9/handbagstore/service/impl/BrandServiceImpl.java
+++ src/main/java/com/g9/handbagstore/service/impl/BrandServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.g9.handbagstore.entity.Brand;
 import com.g9.handbagstore.repository.BrandRepository;
 import com.g9.handbagstore.service.BrandService;
 
+@Service
 public class BrandServiceImpl implements BrandService {
 	
 	@Autowired
--- src/main/java/com/g9/handbagstore/service/impl/CartDetailServiceImpl.java
+++ src/main/java/com/g9/handbagstore/service/impl/CartDetailServiceImpl.java
@@ -3,11 +3,13 @@ package com.g9.handbagstore.service.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.g9.handbagstore.entity.CartDetail;
 import com.g9.handbagstore.repository.CartDetailRepository;
 import com.g9.handbagstore.service.CartDetailService;
 
+@Service
 public class CartDetailServiceImpl implements CartDetailService {
 	
 	@Autowired
